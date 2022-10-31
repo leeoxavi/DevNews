14db9ea62bdbd4a7fc7ee7a68e47504f2a2828fb
Leonardo Xavier <101280914+leeoxavi@users.noreply.github.com>
2022-10-31 11:18:55 -0300

    Delete DevNews directory

--- DevNews/src/front/controller/app_validacao.js
+++
@@ -1,9 +0,0 @@
-import {valida} from '../model/script.js'
-
-const inputs = document.querySelectorAll('input');
-
-inputs.forEach(input => {
-    input.addEventListener('blur', (evento) => {
-        valida(evento.target);
-    });
-});
